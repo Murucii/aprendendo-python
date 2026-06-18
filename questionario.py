38655a84ddd5e252051b9e7a3edebfaff44bbd15
Muruci <152042132+Murucii@users.noreply.github.com>
2026-06-18 18:33:05 -0300

    Add basic questionnaire functionality

---
+++ questionario.py
@@ -0,0 +1,8 @@
+print ("=======Questionario=======")
+
+num1 = input("qual seu nome?")
+num2 = input("qual sua idade?")
+
+print ("seu nome é", num1, "e tens", num2, "anos")
+
+print ("==========Fim!============")
