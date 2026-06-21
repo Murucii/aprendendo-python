2c650588aa04a3fb836babe0ab68684f83d8e7b1
Muruci <152042132+Murucii@users.noreply.github.com>
2026-06-21 19:50:27 -0300

    calculo de notas do enem 0.1.py
    aumentei o tamanho do codigo usando as condicionais if, para definir notas minimas para cada area da natureza do enem, com base no edital da ufpa,400 sendo o minimo para cada area tirando redação e 500 sendo o minimo para redação , depois estabeleci uma condicional de print para media, se o usuario ter uma media abaixo de 550 ele recebera uma mensagem negativa e se for uma media acima de 650 sera uma mensagem condicional positiva

--- calculo da media de notas do enem.py
+++ calculo da media de notas do enem.py
@@ -1,15 +1,52 @@
 print ("==========Media do Enem==========")
 
 nota_1 = float(input("nota de Linguagens:"))
+
+if nota_1 < 400:
+    print('não passou')
+
+if nota_1 >= 400:
+    print ('passou nessa.')
                
 nota_2 = float(input("nota de  Humanas:"))
-               
+  
+if nota_2 < 400:
+    print('não passou')
+
+if nota_2 >= 400:
+    print ('passou nessa.')  
+  
 nota_3 = float(input("nota de Naturezas:"))
+
+if nota_3 < 400:
+    print('não passou')
+
+if nota_3 >= 400:
+    print ('passou nessa.')
 
 nota_4 = float(input("nota de Matemática:"))
 
-nota_5 = int(input("Nota da Redação:"))
+if nota_4 < 400:
+    print('não passou')
+
+if nota_4 >= 400:
+    print ('passou nessa.')
+
+nota_5 = float(input("Nota da Redação:"))
+
+if nota_5 < 500:
+    print('não passou')
+
+if nota_5 >= 500:
+    print ('passou nessa.')
 
 media = (nota_1 + nota_2 + nota_3 + nota_4 + nota_5) // 5
 
 print (f"sua nota do enem foi:{media}")
+
+if media <= 550:
+    print ('Lamento dizer... mas voce não passa em muita coisa :<')
+    
+if media >= 650:
+    print ('Voce tem muitas chances!! :D')
+    
