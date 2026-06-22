13da963c210d551a8c832520313b122a7b21f7f8
Muruci <152042132+Murucii@users.noreply.github.com>
2026-06-21 21:23:03 -0300

    atualização da tabela de checkout de produto versão 0.1
    atualizei a tabela de checkout adicionando condicionais if  e else para definir o plural e singular dependendo da escolha do usuario, e adicionei o comando :.2f para ajustar os valores multiplicados para eles ficarem visualmente agradaveis

--- simulador-de-checkout.py
+++ simulador-de-checkout.py
@@ -1,18 +1,30 @@
-#Pergunte ao usuário o nome do produto.
+#nome do produto.
 produto = input ("qual o nome do produto?")
 
-#Pergunte a quantidade desse produto que ele deseja comprar.
+#quantidade desse produto que ele deseja comprar.
 
 quantidade = int(input ("qual a quantidade desse produto que voce deseja comprar?"))
 
-#Pergunte o preço unitário desse produto.
+#preço unitário desse produto.
 
 preço_unitario_do_produto = float(input ("qual o preço unitario do produto?"))
 
-#Calcule o valor total da compra (Quantidade × Preço Unitário).
+print('==========SUA COMPRA===============')
+
+#valor total da compra (Quantidade × Preço Unitário).
 
 valor_total_da_compra = quantidade * preço_unitario_do_produto
 
-print (produto, quantidade, preço_unitario_do_produto)
+print(f'nome:{produto}')
 
-print (valor_total_da_compra)
+if quantidade > 1:
+    print(f'quantidade:{quantidade} unidades')
+else:
+    print(f'quantidade:{quantidade} unidade')
+      
+print(f'preço unitario do produto:{preço_unitario_do_produto: .2f}R$')
+
+print ('==========VALOR DA COMPRA==========')
+
+print (f'Preço total:{valor_total_da_compra: .2f}R$')
+
