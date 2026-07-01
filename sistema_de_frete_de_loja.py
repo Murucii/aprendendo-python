8bb8fc33f1c372056075b2a156f0fc5fe9fc0cca
Muruci <duartearthurmedeiros@gmail.com>
2026-07-01 14:24:41 -0300

    Update sistema_de_frete_de_loja.py with frete grátis checks

--- sistema_de_frete_de_loja.py
+++ sistema_de_frete_de_loja.py
@@ -7,11 +7,15 @@ E o cliente precisa estar cadastrado no programa de fidelidade
 Se as duas condições forem verdadeiras, mostre: "Frete grátis aplicado!"
 Caso contrário: "Frete não disponível gratuitamente."
 '''
+print('====================Checagem de Frete====================')
 valor_da_compra = float(input('Qual o valor da compra?'))
 programa_de_fidelidade = input('Voce esta cadastrado no programa de fidelidade?')
 
 if valor_da_compra >= 100 and programa_de_fidelidade == 'sim':
     print('Frete grátis aplicado!')
+    print('Boa compra! :D')
 else:
     print('Frete não disponível gratuitamente.')
+    print('Pague o valor inteiro.')
+print('====================Fim da Checagem======================')
     
